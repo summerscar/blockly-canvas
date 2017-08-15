5c790310587167a27c2ac0026c434e895f84b82e
summerscar <shen.yus@163.com>
2017-08-15 21:17:47 +0800

    第二版 修改全局合成 仍然有问题...

--- cosBlocks.js
+++ cosBlocks.js
@@ -289,7 +289,7 @@ Blockly.Blocks['fillfont'] = {
             .appendField(new Blockly.FieldDropdown([["Arial","Arial"], ["Microsoft YaHei","Microsoft YaHei"]]), "font");
         this.appendDummyInput()
             .setAlign(Blockly.ALIGN_CENTRE)
-            .appendField("水平对齐")
+            .appendField("垂直对齐")
             .appendField(new Blockly.FieldDropdown([["alphabetic","alphabetic"], ["top","top"], ["hanging","hanging"], ["middle","middle"], ["ideographic","ideographic"], ["bottom","bottom"]]), "textBaseline ");
         this.appendDummyInput()
             .setAlign(Blockly.ALIGN_CENTRE)
